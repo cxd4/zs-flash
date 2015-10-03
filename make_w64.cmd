c38ffeceb1aa227c48559919fb971680fe1f1cd8
unknown <rj150@hotmail.com>
2015-10-03 13:22:41 -0400

    Test with /TP instead of /TC to check for C++ warnings.

--- make_w64.cmd
+++ make_w64.cmd
@@ -7,7 +7,9 @@ set include=/I"%MSVC%\..\..\include\crt"
 
 set src=%CD%
 set output=/OUT:zs.exe
+REM set C_FLAGS=/Wall /GL /O1 /Os /Oi /Ob1 /GS- /TP
 set C_FLAGS=/Wall /GL /O1 /Os /Oi /Ob1 /GS- /TC
+
 set files=%src%\main.c %src%\errors.c %src%\flash_io.c %src%\zs_data.c
 
 %MSVC%\cl.exe /MD %include% %C_FLAGS% %files% %EXTRA_LIBS% /link %output% %libs%
