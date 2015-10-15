be9a20e74df7623aa9709c51e9272c9bf70c7f67
unknown <rj150@hotmail.com>
2015-10-15 13:23:59 -0400

    improved WDK build script to not depend on custom installation

--- make_w64.cmd
+++ make_w64.cmd
@@ -1,17 +1,21 @@
 @ECHO OFF
+TITLE Windows Driver Kit 7.1.0
 
+REM set target=i386
 set target=amd64
-set MSVC=%USERPROFILE%\cmsc\msvc\bin\%target%
-set libs=/LIBPATH:"%MSVC%\..\..\lib\%target%"
-set include=/I"%MSVC%\..\..\include\crt"
+
+set C_LANGUAGE_MODE=/TC
+REM set C_LANGUAGE_MODE=/TP
+
+set DDK=C:\WinDDK\7600.16385.1
+set MSVC=%DDK%\bin\x86\%target%
+set include=/I"%DDK%\inc\crt" /I"%DDK%\inc\api"
+set libs=/LIBPATH:"%DDK%\lib\crt\%target%" /LIBPATH:"%DDK%\lib\wnet\%target%"
 
 set src=%CD%
 set output=/OUT:zs.exe
-REM set C_FLAGS=/Wall /GL /O1 /Os /Oi /Ob1 /GS- /TP
-set C_FLAGS=/Wall /GL /O1 /Os /Oi /Ob1 /GS- /TC
-
+set C_FLAGS=/Wall /GL /O1 /Os /Oi /Ob1 /GS- %C_LANGUAGE_MODE% /Fa
 set files=%src%\main.c %src%\errors.c %src%\flash_io.c %src%\zs_data.c
 
-%MSVC%\cl.exe /MD %include% %C_FLAGS% %files% %EXTRA_LIBS% /link %output% %libs%
-
+%MSVC%\cl.exe /MD %include% %C_FLAGS% %files% /link %output% %libs%
 pause
