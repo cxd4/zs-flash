195edbf309911a6448e6895ee312795b12d89375
unknown <rj150@hotmail.com>
2015-10-22 11:39:22 -0400

    added optional trailing letters to help remember params

--- win32-example.cmd
+++ win32-example.cmd
@@ -22,58 +22,59 @@ zs %SAVE_FILE%^
  "-@ 0 means File 1 receives all the below changes."^
  ^
  "Write on the console which mask Link is currently wearing."^
- -m^
+ -mask^
  ^
- -m 0x00 "Sets our preference:  Link is wearing no mask on his face."^
- -F^
- -F 0^
- -N^
- -N 0x0A0A0A0A0A0A0A0A "File 1's name:  AAAAAAAA"^
- -D^
- -D 2 "Read the GitHub wiki if you want to know what this does. :)"^
- -z^
- -z 0x7FFF^
- -Z^
- -Z -3^
- -f 0 13 "Woodfall:  13 stray fairies"^
- -f 1 14 "Snowhead:  14"^
- -f 2 15^
- -f 3 255 "Ikana:  too many :)"^
- -k 0 63^
- -k 1 127^
- -k 2 128^
- -k 3 255^
- -r 500^
- -R 9001^
- -d 0 0xC0^
- -d 1 0x30^
- -d 2 0x0C^
- -d 3 0x03^
- -p^
- -p 4 "Change this to 0 for Fierce Deity Link.  Again, RTFM on GitHub."^
- -L 0^
- -L 1^
- -L 1 0x130^
- -L 0 0x140^
+ -mask 0x00 "Sets our preference:  Link is wearing no mask on his face."^
+ -Fairy^
+ -Fairy 0^
+ -Name^
+ -Name 0x0A0A0A0A0A0A0A0A "File 1's name:  AAAAAAAA"^
+ -Day^
+ -Day 2 "Read the GitHub wiki if you want to know what this does. :)"^
+ -zeldaTime^
+ -zeldaTime 0x7FFF^
+ -ZeldaVectorTimeRate^
+ -ZeldaVectorTimeRate -3^
+ ^
+ -fairies 0 13 "Woodfall:  13 stray fairies"^
+ -keys 0 9^
+ -dungeonProgress 0 7^
+ -fairies 1 14 "Snowhead:  14"^
+ -keys 1 9^
+ -dungeonProgress 1 7^
+ -fairies 2 15^
+ -keys 2 9^
+ -dungeonProgress 2 7^
+ -fairies 3 255 "Ikana:  too many stray fairies :)"^
+ -keys 3 9^
+ -dungeonProgress 3 7^
+ -keys 9 19 "bug in ROM that reads keys[9] as double-defense heart containers"^
+ ^
+ -rupees 500^
+ -RazorSwordHP 9001^
+ -playAsForm^
+ -playAsForm 4 "Change this to 0 for Fierce Deity Link.  Again, RTFM on wiki."^
+ -LifeHP(current) 1 0x140^
+ -LifeHP(maximal) 0 0x140^
+ -MagicNow 1 0^
+ -MagicMax 0 +127^
  -M 1^
  -M 0^
- -M 1 0^
- -M 0 0x7F^
- -o 1000001111111111^
- -U^
- -U 0xFFFFE01B^
+ -owlStatues 1000001111111111^
+ -Upgrades^
+ -Upgrades 0xFFFFE01B^
  ^
- -l 0 4 2 0 "Day 1 winning lottery ticket numbers:  420"^
- -l 1 4 2 0 "Day 2 winning lottery ticket numbers:  420"^
- -l 2 4 2 0^
+ -lottery 0 4 2 0^
+ -lottery 1 4 2 0^
+ -lottery 2 4 2 0^
  ^
- -E 0x0022^
+ -EquipSwordAndShield 0x0034^
  ^
  -I it 0 0 00 -I it 1 0 01 -I it 2 0 02 -I it 3 0 03 -I it 4 0 04 -I it 5 0 05^
  -I it 0 1 06 -I it 1 1 07 -I it 2 1 08 -I it 3 1 09 -I it 4 1 0A -I it 5 1 0B^
  -I it 0 2 0C -I it 1 2 0D -I it 2 2 0E -I it 3 2 0F -I it 4 2 10 -I it 5 2 11^
  -I it 0 3 12 -I it 1 3 16 -I it 2 3 20 -I it 3 3 25 -I it 4 3 27 -I it 5 3 15^
- -I masks^
+ -Inventory masks^
  ^
  -i 1 50 "Have 50 arrows."^
  -i 6 40 -i 7 40 "Have 40 bombs and bombchu."^
